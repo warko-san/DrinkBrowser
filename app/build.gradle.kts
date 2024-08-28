c0a7abb892861db0a809ce70e99046f1fa8a3f35
Warko <swartalf222@gmail.com>
2024-08-28 23:14:30 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.serialization)
+}
+
+android {
+    namespace = "com.warko.drinkbrowser"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.warko.drinkbrowser"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    hilt {
+        enableAggregatingTask = true
+    }
+}
+
+dependencies {
+    // Core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+
+    // Lifecycle
+    implementation(libs.lifecycle)
+    implementation(libs.lifecycle.compose)
+
+    // DI
+    implementation(libs.hilt)
+    implementation(libs.hilt.navigation.compose)
+    ksp(libs.hilt.compiler)
+
+    // Compose
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.activity.compose)
+    implementation(libs.splash.screen)
+
+    // Navigation
+    implementation(libs.navigation.compose)
+    implementation(libs.navigation.common)
+
+    // Serialization
+    implementation(libs.serialization)
+
+    // Network
+    implementation(libs.retrofit)
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/warko/drinkbrowser/app/DrinkApplication.kt
@@ -0,0 +1,13 @@
+package com.warko.drinkbrowser.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DrinkApplication : Application() {
+
+//    override fun onCreate() {
+//        super.onCreate()
+//    }
+
+}
